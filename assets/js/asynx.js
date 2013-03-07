0430a97d80fd9d3ca25414d14ef25a549cf86681
Baldwin A. Martinez <baldwin.a.martinez@gmail.com>
2013-03-06 22:04:37 -0500

    Dynamic jQuery

---
+++ assets/js/asynx.js
@@ -0,0 +1,21 @@
+
+
+
+$('#viewEdit').OnClick(function(){
+
+	
+
+//---------------------------------Scrap Code----------------------------------------------------------------------------->
+
+/*
+		var auth = $('#auth').val();
+		var subj = $('#subj').val();
+		var post = $('#post').val();
+		$('#notifier').html("<div align='center' style='width: auto;'><span style='color: green;'>Processing...<img src='ajax-loader.gif'/></span></div>")
+		$.post('Tweetz.php', {auth: auth, subj: subj, post: post}, function(data){
+		$('#tweets').html(data);
+		$('#notifier').html("");
+		})
+
+*/
+	})
